1e83304580eea801c43a02414c224c03603472e3
Filip Mikina <filipmikina@gmail.com>
2025-07-15 22:43:22 +0200

    add ansible linter, run it on precommit hooks, adjust linting issues

---
+++ hooks/ansible_lint.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#!/bin/bash
+
+set -e
+
+source venv/bin/activate
+
+dirs=("services/core" "bootstrap/")
+errors=()
+
+for dir in "${dirs[@]}"; do
+  echo "Running ansible-lint in $dir..."
+  if ! (cd "$dir" && ansible-lint --fix --offline); then
+    errors+=("$dir")
+  fi
+done
+
+if [ "${#errors[@]}" -ne 0 ]; then
+  echo -e "\n❌ Ansible-lint failed in the following directories:"
+  for err in "${errors[@]}"; do
+    echo "  - $err"
+  done
+  exit 1
+else
+  echo -e "\n✅ All ansible-lint checks passed!"
+fi
