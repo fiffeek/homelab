a97cd612f43398e08d40d0d3eb1999c2df654e58
Filip Mikina <filipmikina@gmail.com>
2025-07-16 14:19:03 +0200

    add shellcheck, ignore info errors

--- scripts/oneshots/get_caddy_root_cert.sh
+++ scripts/oneshots/get_caddy_root_cert.sh
@@ -6,5 +6,5 @@ CERT_LOCATION="/home/fmikina/storage/docker-data/core-apps/caddy/data/data/caddy
 CADDY_HOST="cherry-pot.home"
 CERTS_LOCAL_DIR="$HOME/certs/caddy"
 
-mkdir -p $CERTS_LOCAL_DIR
-ssh ${USER}@${CADDY_HOST} "sudo cat ${CERT_LOCATION}" >"${CERTS_LOCAL_DIR}/root.crt"
+mkdir -p "$CERTS_LOCAL_DIR"
+ssh "${USER}"@${CADDY_HOST} "sudo cat ${CERT_LOCATION}" >"${CERTS_LOCAL_DIR}/root.crt"
